1db1f884148848ead7e58c555fb06a552d46335b
zhubeilife <zhubeilife@gmail.com>
2022-04-30 21:22:41 +0800

    implement week3 point.java

--- src/CollinearPoints/Point.java
+++ src/CollinearPoints/Point.java
@@ -8,8 +8,11 @@
  *
  ******************************************************************************/
 
-import java.util.Comparator;
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
 
@@ -59,7 +62,17 @@ public class Point implements Comparable<Point> {
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
-        /* YOUR CODE HERE */
+        if (that.x == this.x && that.y == this.y) {
+            return Double.NEGATIVE_INFINITY;
+        } else if (that.x == this.x) {
+            return Double.POSITIVE_INFINITY;
+        } else if (that.y == this.y) {
+            return 0.0;
+        } else {
+            // TODO
+            // is this the right way to convert to double?
+            return (double) (that.y - this.y) / (that.x - this.x);
+        }
     }
 
     /**
@@ -75,7 +88,13 @@ public class Point implements Comparable<Point> {
      *         argument point
      */
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
+        if (that.y == this.y) {
+            return Integer.compare(this.x, that.x);
+        } else if (this.y > that.y) {
+           return 1;
+        } else {
+            return -1;
+        }
     }
 
     /**
@@ -85,9 +104,15 @@ public class Point implements Comparable<Point> {
      * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        /* YOUR CODE HERE */
+        return new BySlope();
     }
 
+    private class BySlope implements Comparator<Point>
+    {
+        public int compare(Point a, Point b) {
+            return Double.compare(Point.this.slopeTo(a), Point.this.slopeTo(b));
+        }
+    }
 
     /**
      * Returns a string representation of this point.
@@ -105,6 +130,38 @@ public class Point implements Comparable<Point> {
      * Unit tests the Point data type.
      */
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
+        int x0 = Integer.parseInt(args[0]);
+        int y0 = Integer.parseInt(args[1]);
+        int n = Integer.parseInt(args[2]);
+
+        StdDraw.setCanvasSize(800, 800);
+        StdDraw.setXscale(0, 100);
+        StdDraw.setYscale(0, 100);
+        StdDraw.setPenRadius(0.005);
+        StdDraw.enableDoubleBuffering();
+
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++){
+            int x = StdRandom.uniform(100);
+            int y = StdRandom.uniform(100);
+            points[i] = new Point(x, y);
+            points[i].draw();
+        }
+
+        // draw p = (x0, x1) in red
+        Point p = new Point(x0, y0);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.setPenRadius(0.02);
+        p.draw();
+
+        // draw line segments from p to each point, one at a time, in polar order
+        StdDraw.setPenRadius();
+        StdDraw.setPenColor(StdDraw.BLUE);
+        Arrays.sort(points, p.slopeOrder());
+        for (int i = 0; i < n; i++) {
+            p.drawTo(points[i]);
+            StdDraw.show();
+            StdDraw.pause(100);
+        }
     }
 }
